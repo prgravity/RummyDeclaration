c7ed4100f4670d1d596d4b2bf761ab55550a823a
Priya Khandelwal <priya.khandelwal2010@vit.ac.in>
2014-08-06 11:24:32 +0530

    Update RummyGame.java

--- RummyGame.java
+++ RummyGame.java
@@ -27,6 +27,36 @@ class Hand{
         threeSets = 0;
     }
     
+    public Hand(Hand hand){
+        this.game = hand.game;
+        this.fourRuns = 0;
+        this.threeRuns = 0;
+        this.threeSets = 0;
+    }
+    
+    public Hand(int[] cards){
+        game = new int[13][4];
+        fourRuns = 0;
+        threeRuns = 0;
+        threeSets = 0;
+        
+        for (int i = 0; i < cards.length; i++) {
+            int card = cards[i % 52];
+            int rank = card % 13;
+            int suit = card / 13;
+            game[rank][suit] = 1;
+        }
+    }
+    
+    public void printHand(){
+        for(int i = 0; i < 13; i++){
+            for(int j = 0; j < 4; j++){
+                System.out.print(game[i][j]+"\t");
+            }
+            System.out.println();
+        }
+    }
+    
     public void initializeCardsRandomly(int n){
         Random rgen = new Random();
         int[] cards = new int[52];
@@ -44,13 +74,13 @@ class Hand{
 
         for (int i = 0; i < n; i++) {
             int card = cards[i % 52];
-            int rank = card % 13 + 1;
-            int suit = card / 13 + 1;
-            game[rank - 1][suit - 1] = 1;
+            int rank = card % 13;
+            int suit = card / 13;
+            game[rank][suit] = 1;
         }
     }
     
-    public void getRuns(){
+    public void getRunsOfFour(){
         for(int i = 0; i < 4; i++){
             for( int j = 0; j < 13; j++){
                 if(j + 3 < 13 && fourRuns == 0 && game[j][i] == 1 && game[j+1][i] == 1 && game[j+2][i] == 1 && game[j+3][i] == 1){
@@ -60,7 +90,14 @@ class Hand{
                     game[j + 3][i] = 0;
                     fourRuns++;
                 }
-                else if(threeRuns < 3 && j + 2 < 13 && game[j][i] == 1 && game[j + 1][i] == 1 && game[j + 2][i] == 1){
+            }
+        }
+    }
+    
+    public void getRunsOfThree(){
+        for(int i = 0; i < 4; i++){
+            for( int j = 0; j < 13; j++){
+                if(threeRuns < 3 && j + 2 < 13 && game[j][i] == 1 && game[j + 1][i] == 1 && game[j + 2][i] == 1){
                     game[j][i] = 0;
                     game[j + 1][i] = 0;
                     game[j + 2][i] = 0;
@@ -70,7 +107,7 @@ class Hand{
         }
     }
     
-        public void getSets(){
+    public void getSetsOfThree(){
         for(int i = 0; i < 13; i++){
             for( int j = 0; j < 4; j++){
                 if(j + 2 < 4 && threeSets < 3 && game[i][j] == 1 && game[i][j+1] == 1 && game[i][j+2] == 1){
@@ -157,14 +194,31 @@ class Hand{
         }
         return count;
     }
+    
+    public Hand getOptimalSolution(Hand hand){
+        Hand newHand = new Hand(hand);
+        hand.getRunsOfFour();
+        hand.getSetsOfThree();  //preference to sets of three
+        hand.getRunsOfThree();
+        
+        newHand.getRunsOfFour();
+        newHand.getRunsOfThree(); //preference to runs of three
+        newHand.getSetsOfThree();
+        
+        if(newHand.getRemainingCards() > hand.getRemainingCards())
+            return hand;
+        return newHand;
+    }
 }
 
 public class RummyGame {
     public static void main(String args[]){
-        Hand hand = new Hand();
-        hand.initializeCardsRandomly(13);
-        hand.getRuns();
-        hand.getSets();
+        int[] cards = {0,1,2,3,13,14,15,16,26,27,28,29,39};
+        Hand hand = new Hand(cards);
+        hand.printHand();
+        //hand.initializeCardsRandomly(13);
+        hand = hand.getOptimalSolution(hand);
+        
         System.out.println("Runs Of Four: "+hand.fourRuns+" Runs Of Three "+hand.threeRuns+" Sets Of Three: "+hand.threeSets);
         System.out.println("Number of cards needed: "+ hand.getRemainingCards());
     }
